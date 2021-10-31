f4163173c1ae6fbef15da620712bfc762906fc16
Alan T <55752172+alanctnk@users.noreply.github.com>
2021-10-31 19:13:23 -0300

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config()
-console.log(process.env.USERNAME)
+
 const connection = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USERNAME,
